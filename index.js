aae9fa62e6b230214d99672739767894291c9de9
王镇 <hero21c@outlook.com>
2017-03-25 18:12:42 +0800

    新增 width属性

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-/**
- * Created by simon on 2017/3/15.
- */
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-export default class Divider extends Component {
-  static propTypes = {
-    height: React.PropTypes.number,
-    color: React.PropTypes.string
-  }
+import BaseComponent from './BaseComponent'
 
-  render() {
-    const height = this.props.height
-    const color = this.props.color
+export default class Divider extends BaseComponent {
+    static propTypes = {
+        height: React.PropTypes.number,
+        color: React.PropTypes.string,
+        width: React.PropTypes.number
+    }
 
-    const customDivider = {}
-    _.assign(customDivider,
-      height ? { height: height } : {},
-      color ? { backgroundColor: color } : {}
-    )
+    render() {
+        const height = this.props.height
+        const color = this.props.color
 
-    return (
-      <View style={StyleSheet.flatten([styles.divider, customDivider])}/>
-    )
-  }
+        const customDivider = {}
+        _.assign(customDivider,
+                 height ? { height: height } : {},
+                 color ? { backgroundColor: color } : {},
+                 this.props.width ? { width: this.props.width } : {}
+                )
+
+        return (
+                <View style={StyleSheet.flatten([styles.divider, customDivider, this.props.style])}/>
+        )
+    }
 }
 
 const styles = {
-  divider: {
-    backgroundColor: '#D5D5D5',
-    height: 0.5
-  }
+    divider: {
+        backgroundColor: '#D5D5D5',
+        height: 0.5
+    }
 }
